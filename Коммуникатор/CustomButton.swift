d135a2f80e430cc9ec39dcd62e1c35e3b09199bf
lozinskaya <lozinskaya21@rambler.ru>
2020-11-23 20:43:13 +0500

    Project

---
+++ Коммуникатор/CustomButton.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButton.swift
+//  Коммуникатор
+//
+//  Created by Наталья Лозинская on 09.11.2020.
+//
+
+import Foundation
+import UIKit
+
+class CustomButton: UIButton {
+
+    
+
+
+}
+
---
+++ Коммуникатор/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Коммуникатор
+//
+//  Created by Наталья Лозинская on 08.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
