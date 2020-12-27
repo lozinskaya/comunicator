1c0634042cc15c103c54de26f2738987767397f9
lozinskaya <lozinskaya21@rambler.ru>
2020-12-27 18:52:11 +0500

    alert

--- Коммуникатор/ProfileController.swift
+++ Коммуникатор/ProfileController.swift
@@ -11,6 +11,17 @@ import UIKit
 class ProfileController: UIViewController {
     static var current : ProfileController = ProfileController();
     static var showed = false;
+    
+    @IBAction func ClikName(_ sender: Any) {
+        let alert = UIAlertController(title: "Изменение имени", message: "Необходимо обратиться к администратору “Коммуникатора”", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Так и сделаю", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func ClikEmail(_ sender: Any) {
+        let alert = UIAlertController(title: "Изменение E-mail", message: "Необходимо обратиться к администратору “Коммуникатора”", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Так и сделаю", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
@@ -26,6 +37,8 @@ class ProfileController: UIViewController {
         reloadBalance();
         emailLabel.text = Global.userinfo["email"] as? String;
     }
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
 
