96d24ad9887047a1e9f557cc358060eacb7d052b
lozinskaya <lozinskaya21@rambler.ru>
2021-01-10 21:52:42 +0500

    finish

---
+++ Коммуникатор/FinishController.swift
@@ -0,0 +1,19 @@
+//
+//  FinishController.swift
+//  Коммуникатор
+//
+//  Created by Наталья Лозинская on 10.01.2021.
+//
+
+import UIKit
+
+class FinishController: UIViewController {
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
