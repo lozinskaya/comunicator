14e31cf78990cd73c2a9e4a225befe398b375fea
lozinskaya <lozinskaya21@rambler.ru>
2021-01-11 08:31:55 +0500

    new

---
+++ Коммуникатор/EventsController.swift
@@ -0,0 +1,18 @@
+//
+//  EventsController.swift
+//  Коммуникатор
+//
+//  Created by Наталья Лозинская on 11.01.2021.
+//
+
+import UIKit
+
+class EventsController: UIViewController {
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
