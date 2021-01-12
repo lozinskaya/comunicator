d2318b4563674cfc78309281f951d4de431e9ee0
lozinskaya <lozinskaya21@rambler.ru>
2021-01-12 13:31:37 +0500

    finishControll

--- Коммуникатор/FinishController.swift
+++ Коммуникатор/FinishController.swift
@@ -10,7 +10,10 @@ import UIKit
 class FinishController: UIViewController {
     
    
+    @IBOutlet weak var finishMoney: UILabel!
+    @IBOutlet weak var finishTime: UILabel!
     
+    @IBOutlet weak var clickFinish: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
